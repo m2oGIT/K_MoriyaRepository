90317c771792f72fe30e3c54e9733a56cf2ce59e
Moriya Kouichi <k-moriya@eiwa.com>
2015-11-29 23:05:05 +0900

    新規クラス追加Java001_Test01_2015/11/29(23:03) 新規クラス追加Java001_Test02_2015/11/29(23:03)

---
+++ Practice_moriya/src/Java001/Test02.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+package Java001;
+
+/**
+ * @author （moriya） <br />
+ *  （Java001.Test02） <br />
+ *  （【m2o】理解度確認_演習小問題(Java).Java001.） <br />
+ *  更新履歴 2015/11/29 （moriya）：（新規作成） <br />
+ */
+public class Test02 {
+  public static void main( String[] args ) {
+    System.out.println( "Hello\nWorld!!" );
+  }
+}
