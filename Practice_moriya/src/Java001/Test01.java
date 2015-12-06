41db52b375ca0b27e6295edfab6352badbd48895
Moriya Kouichi <k-moriya@eiwa.com>
2015-12-06 23:55:06 +0900

    Test5追加_2015/12/06(23:55)

--- Practice_moriya/src/Java001/Test01.java
+++ Practice_moriya/src/Java001/Test01.java
@@ -1,16 +1,23 @@
 /**
  * Copyright 2015 EIS Co., Ltd. All rights reserved.
  */
+
 package Java001;
 
 /**
  * @author （moriya） <br />
- *  （Java001.Test01） <br />
- *  （【m2o】理解度確認_演習小問題(Java).Java001.Test01） <br />
- *  更新履歴 2015/11/29 （moriya）：（新規作成） <br />
+ *         （Java001.Test01） <br />
+ *         （【m2o】理解度確認_演習小問題(Java).Java001.Test01） <br />
+ *         更新履歴 2015/11/29 （moriya）：（新規作成） <br />
+ */
+/**
+ * @author （作成者） <br />
+ *         （クラス論理名） <br />
+ *         （説明） <br />
+ *         更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
  */
 public class Test01 {
-  public static void main( String[] args ){
-    System.out.println("Hello World!!");
-   }
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!!" );
+  }
 }
--- Practice_moriya/src/Java001/Test05.java
+++ Practice_moriya/src/Java001/Test05.java
@@ -6,14 +6,14 @@ package Java001;
 
 /**
  * @author （moriya） <br />
- *         （Java001.Test04） <br />
+ *         （Java001.Test05） <br />
  *         （【m2o】理解度確認_演習小問題(Java).Java001.Test04） <br />
- *         更新履歴 2015/11/29 （moriya）：（新規作成） <br />
+ *         更新履歴 2015/12/06 （moriya）：（新規作成） <br />
  */
-public class Test04 {
+public class Test05 {
   public static void main( String[] args ) {
     System.out.println( "Javaの演習問題です。" );
-    System.out.println( args[0] );
+    System.out.println( args[0] + "\n" + args[1] );
     System.out.println( "よろしくお願いします。" );
   }
 }
